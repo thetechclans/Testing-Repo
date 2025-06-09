0d928ff3d06c2231359d9ff2c64e3a644f6f9e31
Majith Barakath <majith@amazetechclans.com>
2025-06-09 13:22:33 +0530

    Update accordion.tsx

--- accordion.tsx
+++ accordion.tsx
@@ -4,6 +4,8 @@
 
 Testing the file: accordion.tsx
 
+testing the git
+
 import * as React from "react"
 import * as AccordionPrimitive from "@radix-ui/react-accordion"
 import { ChevronDown } from "lucide-react"
